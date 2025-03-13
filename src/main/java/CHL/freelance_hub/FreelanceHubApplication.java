2b021cd4500ab8f59345e2fa757d87131ba354b4
ppp16bit <pcampossobrinho@gmail.com>
2025-03-13 10:09:03 -0400

    first commit: config

---
+++ src/main/java/CHL/freelance_hub/FreelanceHubApplication.java
@@ -0,0 +1,13 @@
+package CHL.freelance_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelanceHubApplication.class, args);
+	}
+
+}
