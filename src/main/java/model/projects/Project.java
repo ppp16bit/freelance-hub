8a0c44ce42c0e9ae29271c18cae5ec39024a0423
ppp16bit <pcampossobrinho@gmail.com>
2025-03-17 23:29:49 -0400

    chore: creating freelancer of type User and generating getter and setter methods manually

--- src/main/java/model/projects/Project.java
+++ src/main/java/model/projects/Project.java
@@ -45,4 +45,74 @@ public class Project {
     @ManyToOne
     @JoinColumn(name = "client_id", nullable = false)
     private User customer;
+
+    @ManyToOne
+    @JoinColumn(name = "freelancer_id", nullable = false)
+    private User freelancer;
+
+    @NotNull(message = "Budget is mandatory")
+    @PositiveOrZero(message = "Budget must be positive or zero")
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(@NotNull(message = "Budget is mandatory") @PositiveOrZero(message = "Budget must be positive or zero") double budget) {
+        this.budget = budget;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public User getFreelancer() {
+        return freelancer;
+    }
+
+    public void setFreelancer(User freelancer) {
+        this.freelancer = freelancer;
+    }
+
+    public @NotBlank(message = "Description is mandatory") String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NotBlank(message = "Description is mandatory") String description) {
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public @NotNull(message = "Status is mandatory") ProjectStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(@NotNull(message = "Status is mandatory") ProjectStatus status) {
+        this.status = status;
+    }
+
+    public @NotNull(message = "Deadline is mandatory") LocalDate getTerm() {
+        return term;
+    }
+
+    public void setTerm(@NotNull(message = "Deadline is mandatory") LocalDate term) {
+        this.term = term;
+    }
+
+    public @NotBlank(message = "Title is mandatory") String getTitle() {
+        return title;
+    }
+
+    public void setTitle(@NotBlank(message = "Title is mandatory") String title) {
+        this.title = title;
+    }
 }
